f24090f843aff2206fb47c3430717d43818df799
vdeaugustine <vinnied44@csu.fullerton.edu>
2023-07-08 11:13:41 -0700

    Added different version of previous Binding extension

--- Sources/Vin/Extensions/SwiftUI/Framework Extensions/SwiftUIFrameworkExtensions.swift
+++ Sources/Vin/Extensions/SwiftUI/Framework Extensions/SwiftUIFrameworkExtensions.swift
@@ -29,4 +29,28 @@ public extension Binding {
             closure()
         })
     }
+    
+    /**
+       Registers a closure to be executed when the value of the binding is updated **but only if** the *newValue* is *equal to* the *previous value*, and optionally a completion closure to be executed after the value is set.
+
+       - Parameters:
+            - closureForNoChange: A closure that takes the updated value as its parameter and is executed if the new value is equal to the current value of the binding.
+            - completion: An optional closure to be executed after the value is set.
+
+       - Returns: A new binding that wraps the original value.
+
+       - Note: The closure `closureForNoChange` will only be executed if the new value is equal to the current value of the binding. The `completion` closure, if provided, will always be executed after the value is set.
+    */
+    
+    func onUpdate(ifNoChange closureForNoChange: @escaping (Value) -> Void,
+                  completion: (() -> Void)? = nil) -> Binding<Value> where Value: Equatable {
+        Binding(get: { wrappedValue },
+                set: {
+                    if $0 == wrappedValue {
+                        closureForNoChange($0)
+                    }
+                    wrappedValue = $0
+                    completion?()
+                })
+    }
 }
